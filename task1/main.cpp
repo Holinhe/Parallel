0363b60ee7f414506749d51f3254b82e5023efaa
Holinhe <putart05@gmail.com>
2025-03-19 11:12:31 +0000

    Finally commit?

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <cmath>
+#define _USE_MATH_DEFINES
 
+#if USE_DOUBLE
+using var = float;
+#else
+using var = double;
+#endif
+using namespace std;
+
+int main()
+{
+    var sum = 0;
+    var array[10^7];
+    for(int i = 0; i < (10^7); i++)
+    {
+        var a = sin(M_PI / (10^7) * i);
+        array[i] = a;
+        sum += a;
+    }
+    cout << sum << endl;
+}
